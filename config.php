c77ae004629d0fea0d36172182b78a18d9cb1ec0
WendyEugenia <wendy.t.n@hotmail.com>
2024-05-22 17:54:19 -0300

    first commit

---
+++ config.php
@@ -0,0 +1,33 @@
+<?php
+
+//     $dbHost = 'localhost';
+//     $dbUsername = 'root';
+//     $dbPassword = 'rootroot';
+//     $dbName= 'igreja_batista3';
+   
+   
+
+//$conexao = new mysqli($dbHost,  $dbUsername,  $dbPassword , $dbName);
+
+//     if (!$conexao -> connect_error) {
+//      echo "Erro";
+
+//     }
+
+//     else {
+//        echo"Conexão efetuada";
+//    }   
+
+$dbHost = 'localhost';
+$dbUsername = 'root';
+$dbPassword = '';
+$dbName = 'igreja_batista3';
+
+$conexao = new mysqli($dbHost, $dbUsername, $dbPassword, $dbName);
+
+if ($conexao->connect_error) {
+    die("Erro de conexão: " . $conexao->connect_error);
+}
+
+?>
+
